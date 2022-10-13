4908b7f51d8fa76d90b52b035bb886033dc3e626
joel-dev22 <joeltmathew1999@gmail.com>
2022-10-14 01:40:51 +0530

    learned about creating, deleting and selecting as well as inserting and adding classes in JS DOM

--- script.js
+++ script.js
@@ -29,3 +29,46 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Selecting Elements
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const btns = document.getElementsByTagName('button');
+console.log(btns);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Deleting Elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies to improve functionality';
+message.innerHTML =
+  'We use cookies to improve functionality <button class="btn btn--close-cookie">Got it!</button>';
+header.append(message);
+// header.prepend(message.cloneNode(true));
+// header.before(message);
+
+// Delete Elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+console.log(getComputedStyle(message));
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
