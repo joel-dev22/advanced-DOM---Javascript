7166fee8e57556c678a7f94d033f5cdff21ec73c
joel-dev22 <joeltmathew1999@gmail.com>
2022-10-26 06:35:42 +0530

    learned how to reveal elements on scroll in JS DOM

--- script.js
+++ script.js
@@ -140,6 +140,24 @@ const headObserver = new IntersectionObserver(stickyNav, {
 });
 headObserver.observe(header);
 
+// Oserve sections
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 // document.querySelectorAll('.nav__link').forEach(el =>
 //   el.addEventListener('click', function (e) {
 //     e.preventDefault();
