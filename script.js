e0177649fba3f5afd051eab3296464d3c0c6fb81
joel-dev22 <joeltmathew1999@gmail.com>
2022-10-27 07:19:56 +0530

    learned about lazy loading of images

--- script.js
+++ script.js
@@ -158,6 +158,33 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // document.querySelectorAll('.nav__link').forEach(el =>
 //   el.addEventListener('click', function (e) {
 //     e.preventDefault();
